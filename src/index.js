7af8403f84300cc756323cb928f183b679ca5ea3
juliaemgould <86896127+juliaemgould@users.noreply.github.com>
2021-07-05 19:25:29 -0500

    Element Type error

--- src/index.js
+++ src/index.js
@@ -22,3 +22,10 @@ ReactDOM.render(
   </FilterContextProvider>,
   document.getElementById("root")
 );
+
+/* I kept getting the following error:
+Element type is invalid: expected a string (for built-in components) or a class/function (for composite components) but got: undefined. 
+You likely forgot to export your component from the file it's defined in, 
+or you might have mixed up default and named imports. 
+
+It resolved when I created a new fork*/
